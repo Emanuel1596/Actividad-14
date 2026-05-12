318bdb4bacaa4770a3787a55905088cdca07783b
fershggs <emanuel.villanueva552@gmail.com>
2026-05-11 21:12:51 -0600

    Actividad 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,3 +35,14 @@ android {
         }
     }
 }
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
--- app/src/main/java/com/example/definitiva/SecondaryActivity.kt
+++ app/src/main/java/com/example/definitiva/SecondaryActivity.kt
@@ -1,7 +1,7 @@
 package com.example.definitiva
 
 import android.app.Activity
-import android.os.Build
+import android.content.Intent
 import android.os.Bundle
 import com.example.definitiva.databinding.ActivitySecondaryBinding
 
@@ -15,38 +15,13 @@ class SecondaryActivity : Activity() {
         binding = ActivitySecondaryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val nombreUsuario = intent.getStringExtra(MainActivity.EXTRA_USER_NAME) ?: "Sin nombre"
-        val edad = intent.getIntExtra(MainActivity.EXTRA_EDAD, 0)
-        val esEstudiante = intent.getBooleanExtra(MainActivity.EXTRA_ES_ESTUDIANTE, false)
-
-        val heroe = obtenerHeroe()
-
-        binding.tvNombreUsuario.text = "Nombre recibido: $nombreUsuario"
-        binding.tvEdad.text = "Edad: $edad"
-        binding.tvEsEstudiante.text = "¿Es estudiante?: ${if (esEstudiante) "Sí" else "No"}"
-
-        binding.tvHeroe.text = if (heroe != null) {
-            """
-            Héroe recibido:
-            Nombre: ${heroe.nombre}
-            Nivel: ${heroe.nivel}
-            Vida: ${heroe.vida}
-            """.trimIndent()
-        } else {
-            "No se recibió ningún héroe."
+        binding.btnAbrirTercera.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
         }
 
-        binding.btnRegresar.setOnClickListener {
+        binding.btnVolverMain.setOnClickListener {
             finish()
-        }
-    }
-
-    private fun obtenerHeroe(): Heroe? {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra(MainActivity.EXTRA_HEROE, Heroe::class.java)
-        } else {
-            @Suppress("DEPRECATION")
-            intent.getParcelableExtra(MainActivity.EXTRA_HEROE)
         }
     }
 }
